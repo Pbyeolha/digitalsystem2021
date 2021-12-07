93d5f7afa7adc906c11cea1f8e5cee8719799dd8
byeolha Park <park_byeolha@naver.com>
2021-12-07 21:34:13 +0900

    별하

--- random1.v
+++ random1.v
@@ -1,13 +1,13 @@
-module random(
+module random1(
     input rst,
     input clk,
     input[19:0] seed,
-    output [8:0] rand_num0,
-    output [8:0] rand_num1,
-    output [8:0] rand_num2,
-    output [8:0] rand_num3,
-    output [8:0] rand_num4,
-    output [8:0] rand_num5
+    output [7:0] rand_num0,
+    output [7:0] rand_num1,
+    output [7:0] rand_num2,
+    output [7:0] rand_num3,
+    output [7:0] rand_num4,
+    output [7:0] rand_num5
 
     );
     
@@ -33,7 +33,8 @@ wire [1:0] fd_back0, fd_back1, fd_back2, fd_back3, fd_back4, fd_back5;
     assign fd_back5[0] = sreg0[18] ^ sreg0[3];
     assign fd_back5[1] = sreg0[11] ^ sreg0[8];
 
-   
+
+
     always @ (posedge clk) begin
         if(rst) begin 
             sreg0 <= seed;
@@ -42,6 +43,8 @@ wire [1:0] fd_back0, fd_back1, fd_back2, fd_back3, fd_back4, fd_back5;
             sreg3 <= seed;
             sreg4 <= seed;
             sreg5 <= seed;
+
+
         end
         else begin
             sreg0 <= {fd_back0, sreg0[19:2]};
@@ -50,14 +53,18 @@ wire [1:0] fd_back0, fd_back1, fd_back2, fd_back3, fd_back4, fd_back5;
             sreg3 <= {fd_back3, sreg3[19:2]};
             sreg4 <= {fd_back4, sreg4[19:2]};
             sreg5 <= {fd_back5, sreg5[19:2]};
+
+
         end
     end
     
-    assign rand_num0 = sreg0[8:0];
-    assign rand_num1 = sreg1[10:2];
-    assign rand_num2 = sreg2[12:4];
-    assign rand_num3 = sreg3[13:5];
-    assign rand_num4 = sreg4[17:9];
-    assign rand_num5 = sreg5[19:11];
+    assign rand_num0 = sreg0[7:0];
+    assign rand_num1 = sreg1[10:3];
+    assign rand_num2 = sreg2[13:6];
+    assign rand_num3 = sreg3[15:8];
+    assign rand_num4 = sreg4[18:11];
+    assign rand_num5 = sreg5[19:12];
 
+
+    
 endmodule
